2d97cf4f9f750770256f760b3b567704ff384644
gsophy <georgesophy@gmail.com>
2019-10-07 15:05:47 -0400

    Create the s3 bucket for remote state management

---
+++ ch3-terraform-state/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = us-east-1
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "soco-remote-state"
+
+#Prevent accidential deletion of this s3 bucket
+lifecycle {
+    prevent_destroy = true
+    }
+
+#Enable versioning so we can see the full revision history of the state file
+versioning {
+    enabled = true
+}
+
+#Enable server-side encryption by default
+server_side_encryption_configuration {
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+            }
+        }
+    }
+}
+
