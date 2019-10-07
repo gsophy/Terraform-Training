651450aab42eab106cd7f26dbf415bb2de7ee8b5
gsophy <georgesophy@gmail.com>
2019-10-07 15:12:35 -0400

    Added Lock Table for remote state management

--- ch3-terraform-state/main.tf
+++ ch3-terraform-state/main.tf
@@ -25,3 +25,13 @@ server_side_encryption_configuration {
     }
 }
 
+resource "aws_dynamodb_table" "terraform-locks" {
+  name = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+      name = "LockID"
+      type = "S"
+  }
+}
