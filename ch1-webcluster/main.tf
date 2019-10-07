6fbe07c7b31a8bf6f2959e7945a07129f5a02ef5
gsophy <georgesophy@gmail.com>
2019-10-07 12:36:26 -0400

    Added ALB security groups and associated to ALB

--- ch1-webcluster/main.tf
+++ ch1-webcluster/main.tf
@@ -59,6 +59,7 @@ resource "aws_lb" "example-loadbalancer" {
   name = "terraform-asg-example"
   load_balancer_type = "application"
   subnets = data.aws_subnet_ids.default.ids
+  security_groups = [aws_security_group.alb.id]
 }
 
 resource "aws_alb_listener" "http" {
@@ -75,6 +76,27 @@ resource "aws_alb_listener" "http" {
       }
   }
 }
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  #Allow inbound HTTP requests 
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #Allow all outbound requests
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 
 
 
