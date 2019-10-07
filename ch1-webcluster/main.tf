7b859087c2b4f1a16f2b0912f28c1036830f1ad2
gsophy <georgesophy@gmail.com>
2019-10-07 13:56:34 -0400

    Completed code for Web Cluster Exercise

--- ch1-webcluster/main.tf
+++ ch1-webcluster/main.tf
@@ -35,6 +35,10 @@ lifecycle {
 resource "aws_autoscaling_group" "example-ASG" {
   launch_configuration = aws_launch_configuration.example-launch-config.name
   vpc_zone_identifier = data.aws_subnet_ids.default.ids
+  
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
   min_size = 2
   max_size = 10
 
@@ -62,17 +66,17 @@ resource "aws_lb" "example-loadbalancer" {
   security_groups = [aws_security_group.alb.id]
 }
 
-resource "aws_alb_listener" "http" {
-  load_balancer_arn = aws.lb.example-loadbalancer.arn
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example-loadbalancer.arn
   port = 80
   protocol = "HTTP"
   # By default, retun a simple 404 page
   default_action {
       type = "fixed-response"
-      fixed-response {
-          content-message = "text/plain"
-          message-body = "404: Page Not Found"
-          status-code = 404
+      fixed_response {
+          content_type = "text/plain"
+          message_body = "404: Page Not Found"
+          status_code = 404
       }
   }
 }
@@ -94,6 +98,43 @@ resource "aws_security_group" "alb" {
       protocol = "-1"
       cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 15
+        timeout = 3
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+    }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+      field = "path-pattern"
+      values = ["*"]
+  }
+
+  action {
+      type = "forward"
+      target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+output "alb_dns_name" {
+    value = aws_lb.example-loadbalancer.dns_name
+    description = "The Domain Name of the Load Balancer"
 }
 
 
